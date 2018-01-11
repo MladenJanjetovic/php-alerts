d1a4af61ca604e13f13c09182aac2f737fdeece9
Mladen Janjetovic <mladen@tirien.com>
2018-01-11 11:18:28 +0100

    Update Alert.php

--- src/Alert.php
+++ src/Alert.php
@@ -7,82 +7,81 @@
 
 class Alert {
 
-	private static $session_started = false;
+    private static $session_started = false;
 
-	private static function checkSession()
-	{
-		if( !self::$session_started && session_id() == '' )
-		{
-			session_start();
-			self::$session_started = true;
-		}
-	}
-
-    static function show() 
+    private static function checkSession()
     {
-    	self::checkSession();
-
-		if ( !empty($_SESSION["alerts"]) )
-		{ 
-			ksort($_SESSION["alerts"]);
-
-			$alert_type = '';
-			$i = 0;
-
-			foreach ($_SESSION["alerts"] as $key => $alert_group) 
-			{
-				$i++;
-
-				if($alert_type != $key){
-
-					$alert_type = $key;
-
-					if( $i != 1 ){
-						echo '</div>';
-					}
-
-					echo '<div class="alert alert-'.$alert_type.'">';
-
-				}
-
-				foreach ($alert_group as $alert) {
-					echo $alert . "<br>";
-				}
-
-				if( $i == count($_SESSION["alerts"]) ){
-					echo '</div>';
-				}
-			} 
-
-			self::clear();
-		}
+        if( !self::$session_started || session_id() == '' ) {
+            session_start();
+            self::$session_started = true;
+        }
     }
 
-    static function set($type, $alert) 
+    static function show()
     {
-    	self::checkSession();
+        self::checkSession();
 
-    	if (is_array($alert)) 
-    	{
-    		foreach ($alert as $message) {
-    			$_SESSION["alerts"][$type][] = $message;
-    		}
-    	}
-    	else{
-    		$_SESSION["alerts"][$type][] = $alert;
-    	}
+        if ( !empty($_SESSION["alerts"]) )
+        {
+            ksort($_SESSION["alerts"]);
+
+            $alert_type = '';
+            $i = 0;
+
+            foreach ($_SESSION["alerts"] as $key => $alert_group)
+            {
+                $i++;
+
+                if($alert_type != $key){
+
+                    $alert_type = $key;
+
+                    if( $i != 1 ){
+                        echo '</div>';
+                    }
+
+                    echo '<div class="alert alert-'.$alert_type.'">';
+
+                }
+
+                foreach ($alert_group as $alert) {
+                    echo $alert . "<br>";
+                }
+
+                if( $i == count($_SESSION["alerts"]) ){
+                    echo '</div>';
+                }
+            }
+
+            self::clear();
+        }
     }
 
-    static function get() 
+    static function set($type, $alert)
     {
-    	self::checkSession();
-    	return empty($_SESSION["alerts"]) ? array() : $_SESSION["alerts"];
+        self::checkSession();
+
+        if (is_array($alert))
+        {
+            foreach ($alert as $message) {
+                $_SESSION["alerts"][$type][] = $message;
+            }
+        }
+        else{
+            $_SESSION["alerts"][$type][] = $alert;
+        }
     }
 
-    static function clear() 
+    static function get()
     {
-    	self::checkSession();
-    	unset($_SESSION["alerts"]);
+        self::checkSession();
+        return empty($_SESSION["alerts"]) ? array() : $_SESSION["alerts"];
+    }
+
+    static function clear()
+    {
+        self::checkSession();
+        unset($_SESSION["alerts"]);
     }
 
 }
